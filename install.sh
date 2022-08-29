89ccdb2a52a20d11d03c03b441b410cd8d2ed5e1
Stefan <--global>
2022-08-29 09:10:15 -0500

    re-restructure repo

--- install.sh
+++ install.sh
@@ -1,33 +1,57 @@
 #!/usr/bin/env bash
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+function install_homebrew {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
 
-# Install packages
-brew install << PACKAGES
-bat
-diff-so-fancy
-fzf
-gh
-htop
-httpie
-irssi
-mosh
-prettyping
-python
-ranger
-speedtest-cli
-the_silver_searcher
-tmux
-vim
-zsh-syntax-highlighting
+function install_packages {
+    brew install << PACKAGES
+    bat
+    diff-so-fancy
+    fzf
+    gh
+    htop
+    httpie
+    irssi
+    mosh
+    prettyping
+    python
+    ranger
+    speedtest-cli
+    the_silver_searcher
+    tmux
+    vim
+    zsh-syntax-highlighting
 PACKAGES
+}
 
-# Install casks
-brew install --cask << CASKS
-1password
-alfred
-bartender
-docker
-istat-menus
+function install_casks {
+    brew install --cask << CASKS
+    1password
+    alfred
+    bartender
+    docker
+    istat-menus
+    iterm2
+    nova
 CASKS
+}
+
+function copy_configs {
+    rsync \
+        --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude "install.sh" \
+        --exclude "README.md" \
+        -avh --no-perms . $HOME
+}
+
+
+install_homebrew
+install_packages
+install_casks
+
+copy_configs
+
+touch $HOME/.access_tokens
+vim +PlugInstall
