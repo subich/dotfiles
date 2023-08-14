61c2efd5e57a07f9f512f36615d2b0d09247184c
Stefan Subich <20615740+subich@users.noreply.github.com>
2023-08-14 10:09:53 -0400

    add plugins

--- .config/nvim/lua/plugins/git-plugins.lua
+++ .config/nvim/lua/plugins/git-plugins.lua
@@ -1,1 +1,33 @@
-return {}
+return {
+    {
+      "akinsho/git-conflict.nvim",
+      version = "*",
+      config = true,
+    },
+    {
+      "ruifm/gitlinker.nvim",
+      opts = {},
+      init = function()
+        local wk = require("which-key")
+        wk.register({
+          g = {
+            y = "Copy GitHub permalink",
+          },
+        }, { prefix = "<leader>" })
+      end,
+    },
+    {
+      "lewis6991/gitsigns.nvim",
+      opts = {
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          ignore_whitespace = true,
+        },
+        current_line_blame_formatter_opts = {
+          relative_time = true,
+        },
+      },
+    },
+  }
+  
