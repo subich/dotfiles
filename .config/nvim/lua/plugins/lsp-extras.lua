eabc71de216e7289faf0f3f24746583d97457a2d
Stefan Subich <20615740+subich@users.noreply.github.com>
2023-08-14 10:13:24 -0400

    add plugins (#4)

--- .config/nvim/lua/plugins/lsp-extras.lua
+++ .config/nvim/lua/plugins/lsp-extras.lua
@@ -1,1 +1,36 @@
-return {}
+return {
+    { -- display inlay hints from LSP
+      "lvimuser/lsp-inlayhints.nvim", -- INFO only temporarily needed, until https://github.com/neovim/neovim/issues/18086
+      lazy = true, -- required in attach function
+      init = function()
+        vim.api.nvim_create_autocmd("LspAttach", {
+          callback = function(args)
+            local bufnr = args.buf
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            local capabilities = client.server_capabilities
+            if capabilities.inlayHintProvider then
+              require("lsp-inlayhints").on_attach(client, bufnr, false)
+            end
+          end,
+        })
+      end,
+      opts = {
+        inlay_hints = {
+          parameter_hints = {
+            show = true,
+            prefix = "󰁍 ",
+            remove_colon_start = true,
+            remove_colon_end = true,
+          },
+          type_hints = {
+            show = true,
+            prefix = "   ",
+            remove_colon_start = true,
+            remove_colon_end = true,
+          },
+          only_current_line = true,
+          highlight = "NonText", -- highlight group
+        },
+      },
+    },
+  }  
