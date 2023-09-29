7b2d689ca58b505601b396923195d7ab4ee24c3c
Stefan Subich <20615740+subich@users.noreply.github.com>
2023-09-29 19:50:03 -0400

    Update git-plugins.lua

--- .config/nvim/lua/plugins/git-plugins.lua
+++ .config/nvim/lua/plugins/git-plugins.lua
@@ -1,33 +1,43 @@
 return {
-    {
-      "akinsho/git-conflict.nvim",
-      version = "*",
-      config = true,
-    },
-    {
-      "ruifm/gitlinker.nvim",
-      opts = {},
-      init = function()
-        local wk = require("which-key")
-        wk.register({
-          g = {
-            y = "Copy GitHub permalink",
-          },
-        }, { prefix = "<leader>" })
-      end,
-    },
-    {
-      "lewis6991/gitsigns.nvim",
-      opts = {
-        current_line_blame = true,
-        current_line_blame_opts = {
-          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-          ignore_whitespace = true,
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    config = true,
+  },
+  {
+    "ruifm/gitlinker.nvim",
+    opts = {},
+    init = function()
+      local wk = require("which-key")
+      wk.register({
+        g = {
+          y = "Copy GitHub permalink",
         },
-        current_line_blame_formatter_opts = {
-          relative_time = true,
+      }, { prefix = "<leader>" })
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    init = function()
+      local wk = require("which-key")
+      local gs = require("gitsigns")
+      wk.register({
+        g = {
+          b = "Toggle current line blame",
         },
+      }, { prefix = "<leader>" })
+
+      vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame)
+    end,
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        ignore_whitespace = true,
+      },
+      current_line_blame_formatter_opts = {
+        relative_time = true,
       },
     },
-  }
-  
+  },
+}
