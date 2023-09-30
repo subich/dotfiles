407dc0c8b7a72a7c3d54bd6963e104be7ca06fae
Stefan Subich <20615740+subich@users.noreply.github.com>
2023-09-29 20:39:41 -0400

    nvim config updates (#5)

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -4,26 +4,32 @@ local function nullSources()
     -- SPELLING
     builtins.completion.spell,
 
-    -- SHELL
-    builtins.code_actions.shellcheck,
-
     -- LUA
     builtins.formatting.stylua,
 
+    -- SHELL
+    builtins.code_actions.shellcheck,
+
     -- CLOUDFORMATION
     builtins.diagnostics.cfn_lint,
+
+    -- TERRAFORM
+    builtins.diagnostics.terraform_validate,
 
     -- JAVASCRIPT
     builtins.diagnostics.eslint,
 
     -- PYTHON
-    -- builtins.diagnostics.flake8,
     builtins.diagnostics.pylint,
+    -- builtins.diagnostics.mypy,
+    -- builtins.diagnostics.flake8.with({
+    --   extra_args = { "--config", vim.fn.expand("~/.config/flake8") },
+    -- }),
   }
 end
 
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  "nvimtools/none-ls.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = { "mason.nvim" },
   opts = function()
