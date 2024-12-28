876492757ead1ba6da4d08c434df17ea3cffec0c
Stefan <20615740+subich@users.noreply.github.com>
2024-12-28 12:08:58 -0500

    configure snacks dash with git-dashboard-nvim

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,11 +1,33 @@
 return {
-  "nvimdev/dashboard-nvim",
-  event = "VimEnter",
+  "snacks.nvim",
+  ---@type snacks.Config
   dependencies = {
     { "juansalvatore/git-dashboard-nvim", dependencies = { "nvim-lua/plenary.nvim" } },
   },
   opts = function(_, opts)
-    local git_dashboard = require("git-dashboard-nvim").setup({})
-    opts.config.header = git_dashboard
+    local git_dashboard = require("git-dashboard-nvim").setup({
+      top_padding = 0,
+      bottom_padding = 0,
+      fallback_header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+      title = "owner_with_repo_name",
+    })
+
+    -- opts.dashboard.preset.header = table.concat(git_dashboard, "\n")
+    opts.dashboard.sections = {
+      {
+        text = table.concat(git_dashboard, "\n"),
+        align = "center",
+        height = 15,
+        padding = 1,
+      },
+      { section = "keys", gap = 1, padding = 1 },
+      { section = "startup" },
+    }
   end,
 }
