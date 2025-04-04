5dfcc040a3690d6044f074ed3685f43c3575bb8a
Stefan <20615740+subich@users.noreply.github.com>
2025-04-04 09:30:20 -0400

    add neovide configuration

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -5,7 +5,24 @@
 -- Disable LazyVim auto format
 vim.g.autoformat = false
 
+-- Neovide specific options
 vim.o.guifont = "BerkeleyMono Nerd Font:h12"
+
+local change_scale_factor = function(delta)
+  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+end
+
 if vim.g.neovide then
   vim.g.neovide_hide_mouse_when_typing = true
+
+  vim.g.neovide_opacity = 0.8
+  vim.g.neovide_normal_opacity = 0.8
+  vim.g.neovide_scale_factor = 1.0
+
+  vim.keymap.set("n", "<C-=>", function()
+    change_scale_factor(1.1)
+  end, { desc = "Increase Neovide scale factor" })
+  vim.keymap.set("n", "<C-->", function()
+    change_scale_factor(1 / 1.1)
+  end, { desc = "Decrease Neovide scale factor" })
 end
