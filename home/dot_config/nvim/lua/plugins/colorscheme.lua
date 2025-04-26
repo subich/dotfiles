e9f81fe623a69a30224186a645c3a22fd758e3e8
Stefan Subich <20615740+subich@users.noreply.github.com>
2025-04-26 14:28:57 -0400

    update colorschemes

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,15 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
-  { "rebelot/kanagawa.nvim", lazy = true },
-  { "sainnhe/everforest", lazy = true },
+  { "catppuccin/nvim", name = "catppuccin", lazy = true },
   { "ellisonleao/gruvbox.nvim", lazy = true },
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
   { "marko-cerovac/material.nvim", lazy = true },
   { "olimorris/onedarkpro.nvim", lazy = true },
+  { "rebelot/kanagawa.nvim", lazy = true },
   { "rmehri01/onenord.nvim", lazy = true },
+  { "sainnhe/everforest", lazy = true },
 
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "catppuccin" },
+    opts = { colorscheme = "tokyonight" },
   },
 }
