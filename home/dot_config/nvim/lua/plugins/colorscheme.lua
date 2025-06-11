d37c1755e16982b31425d126d1bc59eaa965688c
Stefan Subich <20615740+subich@users.noreply.github.com>
2025-06-11 09:40:57 -0400

    update colorschemes

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,16 @@
 return {
-  { "webhooked/kanso.nvim", lazy = false, priority = 1000 },
-
-  { "EdenEast/nightfox.nvim", lazy = true },
   { "catppuccin/nvim", name = "catppuccin", lazy = true },
-  { "ellisonleao/gruvbox.nvim", lazy = true },
+
   { "folke/tokyonight.nvim", lazy = true },
   { "marko-cerovac/material.nvim", lazy = true },
   { "olimorris/onedarkpro.nvim", lazy = true },
   { "rebelot/kanagawa.nvim", lazy = true },
   { "rmehri01/onenord.nvim", lazy = true },
   { "sainnhe/everforest", lazy = true },
-  { "scottmckendry/cyberdream.nvim", lazy = true },
+  { "webhooked/kanso.nvim", lazy = true },
 
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "kanso" },
+    opts = { colorscheme = "material" },
   },
 }
