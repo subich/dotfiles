5a6a31b056836eefb5e51b6030468f47441026ae
Stefan Subich <stefan.subich@aver.io>
2026-01-27 14:06:49 -0500

    add aliases

--- home/dot_oh-my-zsh/custom/aliases.zsh
+++ home/dot_oh-my-zsh/custom/aliases.zsh
@@ -1,7 +1,32 @@
 # aliases for better versions of some common programs
-command -v bat > /dev/null && alias cat='bat --paging never --decorations never --plain'
 command -v prettyping > /dev/null && alias ping='prettyping --nolegend'
 command -v lazygit > /dev/null && alias lg='lazygit'
+
+# Use bat as pager and cat replacement
+# https://github.com/sharkdp/bat
+if command -v bat >/dev/null 2>&1; then
+  export PAGER="bat"
+  export BAT_THEME="Dracula"
+  alias cat="bat"
+  # alias cat='bat --paging never --decorations never --plain'
+fi
+
+# Setup eza, modern alternative to ls
+# https://github.com/eza-community/eza
+if command -v eza >/dev/null 2>&1; then
+  alias \ls='command ls "$@"'
+  alias ls='eza --icons "$@"'
+  alias ll='eza -lh --icons "$@"'
+  alias la='eza -lha --icons "$@"'
+  alias l='eza -l --icons "$@"'
+  alias lsd='eza -lha --no-permissions --no-user --time-style=relative --icons "$@"'
+  alias lt='eza --tree --icons "$@"'
+  alias lt1='lt --level=1'
+  alias lt2='lt --level=2'
+  alias lt3='lt --level=3'
+  alias lt4='lt --level=4'
+  alias lt5='lt --level=5'
+fi
 
 # useful helper functions
 function run-this { docker compose run --remove-orphans ${PWD##*/} $@ }
@@ -42,4 +67,9 @@ function connect_to_workgroup_admin {
   connection_string="postgresql://${workgroup_name}.${aws_account_id}.us-east-1.redshift-serverless.amazonaws.com:5439/dev"
   PGPASSWORD=$(echo $creds | jq -r '.password') \
   psql $connection_string $user
+}
+
+# Kill everything running on a specific port
+function killport() {
+  kill $(lsof -iTCP -sTCP:LISTEN -n -P | grep $1 | awk '{print $2}')
 }
