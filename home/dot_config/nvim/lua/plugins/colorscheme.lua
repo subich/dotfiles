5d2fc98d367c58bfb646ea90d41d66e5de225293
Stefan S <83985252+stefan-aver@users.noreply.github.com>
2026-04-02 13:08:32 -0400

    Remove sidekick overrides and revert colorscheme to catppuccin-nvim (#35)

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", lazy = true },
-  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
+  { "folke/tokyonight.nvim", lazy = true },
   { "marko-cerovac/material.nvim", lazy = true },
   { "olimorris/onedarkpro.nvim", lazy = true },
 
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "tokyonight" },
+    opts = { colorscheme = "catppuccin-nvim" },
   },
 }
